2feed7f039fc70e00a53bd52bef5e58666add4b1
GiwrgosGln <gergegl1999@gmail.com>
2024-01-10 18:47:46 +0200

    Version 1.0.0

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="bg-slate-800 h-40 flex items-center justify-center text-white">
       <div className="flex items-center space-x-10">
-        <span className="text-xl">
+        <span className="text-md md:text-xl">
           Copyright &copy; {new Date().getFullYear()} All rights are reserved
         </span>
       </div>
